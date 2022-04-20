7e6e66be17f527016afd24ece2ad1deee61b1159
mininjin <mininjin332@gmail.com>
2022-04-20 09:49:37 +0900

    modify docs

--- frontend/src/components/UI/MenuButton.vue
+++ frontend/src/components/UI/MenuButton.vue
@@ -1,4 +1,4 @@
-  <template>
+<template>
   <span class="w-10 h-10 relative">
     <span
       :class="`absolute rounded-xl transition-all duration-200 bg-primary h-1 w-full ${on ? 'translate-y-4.5  rotate-45' : 'translate-y-3'}`"></span>
